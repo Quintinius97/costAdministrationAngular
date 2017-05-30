77204fdea8a2fb434722d1389fab1237baa24bcb
Simon Steinruecken <simon.steinruecken@hpe.com>
2017-05-30 17:13:38 +0200

    much build task

--- gruntfile.js
+++ gruntfile.js
@@ -1,11 +1,11 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     require('load-grunt-tasks')(grunt);
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         watch: {
             sass: {
-                files: "src/assets/style/scss/*.scss",
+                files: "src/*/*.scss",
                 tasks: ['sass']
             }
         },
@@ -15,7 +15,7 @@ module.exports = function(grunt) {
             },
             dist: {
                 files: {
-                    'src/assets/style/css/style.css': 'src/assets/style/scss/style.scss'
+                    'src/style.css': 'src/style.scss'
                 }
             }
         },
@@ -24,7 +24,7 @@ module.exports = function(grunt) {
                 src: ['src/app/**', 'src/assets/style/css/*.*', 'src/index.html']
             },
             options: {
-                watchTask:true,
+                watchTask: true,
                 server: {
                     baseDir: "src"
                 }
@@ -32,22 +32,14 @@ module.exports = function(grunt) {
         },
         copy: {
             css: {
-                files:  [
-                    { src:"node_modules/bootstrap/dist/css/bootstrap.min.css", dest:"src/assets/style/css/bootstrap.min.css"},
-                    { src:"node_modules/angular-material/angular-material.min.css", dest:"src/assets/style/css/angular-material.min.css"},
-                    { src:"node_modules/angular-ui-notification/dist/angular-ui-notification.min.css", dest:"src/assets/style/css/angular-ui-notification.min.css"},
-                    { src:"node_modules/cookieconsent/build/cookieconsent.min.css", dest:"src/assets/style/css/cookieconsent.min.css"}]
+                files: [
+                    { src: "node_modules/bootstrap/dist/css/bootstrap.min.css", dest: "src/dependencies/css/bootstrap.min.css" }
+                ]
             },
             js: {
                 files: [
-                    { src:"node_modules/angular/angular.min.js", dest:"src/assets/js/angular.min.js"},
-                    { src:"node_modules/angular-ui-router/release/angular-ui-router.min.js", dest:"src/assets/js/angular-ui-router.min.js"},
-                    { src:"node_modules/angular-touch/angular-touch.min.js", dest:"src/assets/js/angular-touch.min.js"},
-                    { src:"node_modules/angular-animate/angular-animate.min.js", dest:"src/assets/js/angular-animate.min.js"},
-                    { src:"node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js", dest:"src/assets/js/ui-bootstrap-tpls.js"},
-                    { src:"node_modules/ngstorage/ngStorage.min.js", dest:"src/assets/js/ngStorage.min.js"},
-                    { src:"node_modules/angular-jwt/dist/angular-jwt.min.js", dest:"src/assets/js/angular-jwt.min.js"},
-                    { src:"node_modules/angular-ui-notification/dist/angular-ui-notification.min.js", dest:"src/assets/js/angular-ui-notification.min.js"}
+                    { src: "node_modules/angular/angular.min.js", dest: "src/dependencies/js/angular.min.js" },
+                    { src: "node_modules/angular/angular.min.js", dest: "src/dependencies/js/angular.min.js" },
 
                 ]
             }
@@ -63,7 +55,9 @@ module.exports = function(grunt) {
                     cwd: 'src',
                     src: ['**/*.html', '*.html'],
                     dest: 'dist'
-                }]  }},
+                }]
+            }
+        },
         cssmin: {
             options: {
                 mergeIntoShorthands: false,
@@ -71,7 +65,7 @@ module.exports = function(grunt) {
             },
             target: {
                 files: {
-                   'dist/assets/style/css/style.css': ['src/assets/style/css/angular-ui-notification.min.css', 'src/assets/style/css/cookieconsent.min.css', 'src/assets/style/css/bootstrap.min.css', 'src/assets/style/css/style.css']
+                    'dist/assets/style/css/style.css': ['src/assets/style/css/angular-ui-notification.min.css', 'src/assets/style/css/cookieconsent.min.css', 'src/assets/style/css/bootstrap.min.css', 'src/assets/style/css/style.css']
                 }
             }
         },
@@ -82,14 +76,14 @@ module.exports = function(grunt) {
             my_target: {
                 files: [
                     {
-                        'dist/app/js/script.js' : ['src/app/js/**/*.js']
+                        'dist/app/js/script.js': ['src/app/js/**/*.js']
                     },
                     {
-                    expand: true,
-                    cwd: 'src/assets/js',
-                    src: '**/*.js',
-                    dest: 'dist/assets/js'
-                }]
+                        expand: true,
+                        cwd: 'src/assets/js',
+                        src: '**/*.js',
+                        dest: 'dist/assets/js'
+                    }]
             }
         },
         processhtml: {
@@ -128,7 +122,8 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-imagemin');
 
 
-    grunt.registerTask('do',['imagemin']);
-    grunt.registerTask('default', ['browserSync' , 'copy', 'watch']);
+    grunt.registerTask('do', ['imagemin']);
+    grunt.registerTask('default', ['browserSync', 'copy', 'watch']);
+    grunt.registerTask('copy', ['copy']);
     grunt.registerTask('dist', ['htmlmin', 'cssmin', 'uglify', 'processhtml', 'imagemin']);
 };
