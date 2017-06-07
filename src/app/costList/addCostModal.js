829f0ef02b202141adc688b68f36561a2136f8c1
Simon Steinruecken <simon.steinruecken@hpe.com>
2017-06-07 16:26:41 +0200

    add time (closes #3)

--- src/app/costList/addCostModal.js
+++ src/app/costList/addCostModal.js
@@ -1,21 +1,23 @@
 app.controller('costModalCtrl',
-    ['$scope', 'backend', '$uibModalInstance', function($scope, backend, $uibModalInstance) {
-      $scope.addCost = function() {
-        var payload = {};
-        payload.title = $scope.title;
-        payload.desc = $scope.desc;
-        payload.category = parseInt($scope.category);
-        payload.price = $scope.price;
-        payload.currency = $scope.currency;
-        payload.date = new Date($scope.date).getTime() / 1000;
-        backend.post('cost', payload, function(response) {
-          if(response.status == 200) {
-            console.log('success');
-          } else {
-            console.log('fail');
-          }
-          $uibModalInstance.close();
-        }, "Successful added entry");
-      };
-    }]
+  ['$scope', 'backend', '$uibModalInstance', function ($scope, backend, $uibModalInstance) {
+    $scope.addCost = function () {
+      var payload = {};
+      payload.title = $scope.title;
+      payload.desc = $scope.desc;
+      payload.category = parseInt($scope.category);
+      payload.price = $scope.price;
+      payload.currency = $scope.currency;
+      var date = new Date($scope.date).getTime() / 1000;
+      var time = new Date($scope.time).getTime() / 1000;
+      payload.date = date + time;
+      backend.post('cost', payload, function (response) {
+        if (response.status == 200) {
+          console.log('success');
+        } else {
+          console.log('fail');
+        }
+        $uibModalInstance.close();
+      }, "Successful added entry");
+    };
+  }]
 );
